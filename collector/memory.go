983aae6f84dab82d034ac99a08fd50003f9ec530
Mark Ciril <markciril.jamon@gmail.com>
2026-04-05 13:59:32 +0800

    feat: add context-aware cancellation to memory collector

--- collector/memory.go
+++ collector/memory.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"context"
 	"log"
 	"pulse-agent/models"
 	"pulse-agent/types"
@@ -11,14 +12,19 @@ import (
 
 type MemoryCollector struct{}
 
-func (m MemoryCollector) Collect(ch chan<- models.Metric) {
+func (m MemoryCollector) Collect(ctx context.Context, ch chan<- models.Metric) {
 	for {
-		v, err := mem.VirtualMemory()
-		if err != nil {
-			log.Printf("[Memory] VirtualMemory error: %v", err)
-		} else {
-			ch <- models.Metric{Source: types.MEM, Value: v.UsedPercent}
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			v, err := mem.VirtualMemory()
+			if err != nil {
+				log.Printf("[Memory] VirtualMemory error: %v", err)
+			} else {
+				ch <- models.Metric{Source: types.MEM, Value: v.UsedPercent}
+			}
+			time.Sleep(2 * time.Second)
 		}
-		time.Sleep(2 * time.Second)
 	}
 }
