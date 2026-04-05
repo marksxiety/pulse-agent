629ca1a63a8e7f2ef7d4e3197f47f5fc4fd2be19
Mark Ciril <markciril.jamon@gmail.com>
2026-04-05 13:59:29 +0800

    feat: add context-aware cancellation to CPU collector

--- collector/cpu.go
+++ collector/cpu.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"context"
 	"log"
 	"pulse-agent/models"
 	"pulse-agent/types"
@@ -11,15 +12,19 @@ import (
 
 type CPUCollector struct{}
 
-func (c CPUCollector) Collect(ch chan<- models.Metric) {
+func (c CPUCollector) Collect(ctx context.Context, ch chan<- models.Metric) {
 	for {
-		v, err := cpu.Percent(time.Second, false)
-		if err != nil {
-			log.Printf("[CPU] Percent error: %v", err)
-		} else if len(v) > 0 {
-			ch <- models.Metric{Source: types.CPU, Value: v[0]}
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			v, err := cpu.Percent(time.Second, false)
+			if err != nil {
+				log.Printf("[CPU] Percent error: %v", err)
+			} else if len(v) > 0 {
+				ch <- models.Metric{Source: types.CPU, Value: v[0]}
+			}
+			time.Sleep(2 * time.Second)
 		}
-
-		time.Sleep(2 * time.Second)
 	}
 }
