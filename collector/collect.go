6f14c2023dbc706b3cc50650ffc1602d4b1204b4
Mark Ciril <markciril.jamon@gmail.com>
2026-04-05 13:59:26 +0800

    ref: update Collector interface to accept context.Context for graceful shutdown

--- collector/collect.go
+++ collector/collect.go
@@ -1,8 +1,11 @@
 package collector
 
-import "pulse-agent/models"
+import (
+	"context"
+	"pulse-agent/models"
+)
 
 // this will serve as the interface for all collectors to implement
 type Collector interface {
-	Collect(ch chan<- models.Metric)
+	Collect(ctx context.Context, ch chan<- models.Metric)
 }
