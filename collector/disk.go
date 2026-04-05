8dcfc0af7f61942826036c41cb2049ad7e12be64
Mark Ciril <markciril.jamon@gmail.com>
2026-04-05 13:59:35 +0800

    feat: add context-aware cancellation to disk collector

--- collector/disk.go
+++ collector/disk.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"context"
 	"log"
 	"pulse-agent/models"
 	"pulse-agent/types"
@@ -11,18 +12,23 @@ import (
 
 type DiskCollector struct{}
 
-func (d DiskCollector) Collect(ch chan<- models.Metric) {
+func (d DiskCollector) Collect(ctx context.Context, ch chan<- models.Metric) {
 	for {
-		v, err := disk.Usage("/")
-		if err != nil {
-			log.Printf("Error occurred while collecting disk usage: %v", err)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			v, err := disk.Usage("/")
+			if err != nil {
+				log.Printf("Error occurred while collecting disk usage: %v", err)
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			ch <- models.Metric{
+				Source: types.DISK,
+				Value:  v.UsedPercent,
+			}
 			time.Sleep(1 * time.Second)
-			continue
 		}
-		ch <- models.Metric{
-			Source: types.DISK,
-			Value:  v.UsedPercent,
-		}
-		time.Sleep(1 * time.Second)
 	}
 }
