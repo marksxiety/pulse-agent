4310bf037cdc7f66055b815c707eb53bfe6abe4f
Mark Ciril <markciril.jamon@gmail.com>
2026-04-06 11:01:44 +0800

    feat: add CPU core count via cpu.Counts and emit CPUPayload with percentage and cores

--- collector/cpu.go
+++ collector/cpu.go
@@ -13,6 +13,12 @@ import (
 type CPUCollector struct{}
 
 func (c CPUCollector) Collect(ctx context.Context, ch chan<- models.Metric) {
+
+	cores, err := cpu.Counts(true)
+	if err != nil {
+		cores = 0
+	}
+
 	for {
 		select {
 		case <-ctx.Done():
@@ -22,7 +28,10 @@ func (c CPUCollector) Collect(ctx context.Context, ch chan<- models.Metric) {
 			if err != nil {
 				log.Printf("[CPU] Percent error: %v", err)
 			} else if len(v) > 0 {
-				ch <- models.Metric{Source: types.CPU, Value: v[0]}
+				ch <- models.Metric{
+					Source: types.CPU,
+					Data:   models.CPUPayload{Percentage: v[0], CoreCount: cores},
+				}
 			}
 			time.Sleep(2 * time.Second)
 		}
